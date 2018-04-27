61ed2b792ae8cdf04f369712c13731b772be0007
Ilia Onishchenko <ilia_onishchenko@epam.com>
2018-04-27 14:33:45 +0300

    Pretty working Vocabulary teacher

--- app/binders/PathBinders.scala
+++ app/binders/PathBinders.scala
@@ -2,11 +2,19 @@ package binders
 
 import play.api.i18n.Lang
 import play.api.mvc.PathBindable
+import play.api.mvc.QueryStringBindable
 
 object PathBinders {
 	implicit object LangPathBindable extends PathBindable[Lang] {
 		override def bind(key: String, value: String): Either[String, Lang] = {
 			Lang.get(value).toRight(s"Language $value is not recognized")
+		}
+
+		override def unbind(key: String, value: Lang): String = value.code
+	}
+	implicit object QueryLangBindable extends QueryStringBindable[Lang] {
+		override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, Lang]] = {
+			params.get(key).map(value => Lang.get(value.head).toRight(s"Language ${value.head} is not recognized"))
 		}
 
 		override def unbind(key: String, value: Lang): String = value.code
--- app/controllers/Quiz.scala
+++ app/controllers/Quiz.scala
@@ -1,5 +1,21 @@
 package controllers
 
-class Quiz {
+import play.api.i18n.Lang
+import javax.inject.Inject
 
+import play.api.mvc.Action
+import play.api.mvc.Results._
+import services.VocabularyService
+
+class Quiz @Inject() (vService: VocabularyService) {
+	def quiz(sourceLang: Lang, targetLang: Lang) = Action {
+		vService.findRandomVocabulary(sourceLang, targetLang) match {
+			case Some(_) => Ok
+			case None => Conflict
+		}
+	}
+
+	def check(sourceLang: Lang, word: String, targetLang: Lang, translation: String) = Action {
+		if(vService.verify(sourceLang, word, targetLang, translation)) Ok else NotAcceptable
+	}
 }
