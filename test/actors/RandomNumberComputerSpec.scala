2c6e8e609a0e19d396dde2160e78ea24642e1899
Ilia Onishchenko <ilia_onishchenko@epam.com>
2018-05-15 12:50:32 +0300

    Testing actors for resilience

--- test/actors/RandomNumberComputerSpec.scala
+++ test/actors/RandomNumberComputerSpec.scala
@@ -1,7 +1,8 @@
 package actors
 
 import actors.RandomNumberComputer._
-import akka.actor.ActorSystem
+import akka.actor.SupervisorStrategy.Restart
+import akka.actor.{Actor, ActorRef, ActorSystem, OneForOneStrategy, Props, SupervisorStrategy}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike}
 import org.specs2.matcher.ShouldMatchers
@@ -27,5 +28,28 @@ class RandomNumberComputerSpec(_system: ActorSystem)
       randomNumberComputer ! ComputeRandomNumber(100)
       expectMsgType[RandomNumber]
     }
+  }
+
+  it should "fail when the maximum is a negative number" in {
+
+    class StepParent(target: ActorRef) extends Actor {
+      override def supervisorStrategy: SupervisorStrategy =
+        OneForOneStrategy() {
+          case t: Throwable =>
+            target ! t
+            Restart
+        }
+      override def receive: Receive = {
+        case props: Props => sender() ! context.actorOf(props)
+      }
+    }
+
+    val parent = system.actorOf(Props(new StepParent(testActor)), name = "stepParent")
+
+    parent ! RandomNumberComputer.props
+    val actorUnderTest = expectMsgType[ActorRef]
+    actorUnderTest ! ComputeRandomNumber(-1)
+    expectMsgType[IllegalArgumentException]
+
   }
 }
